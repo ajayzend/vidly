7187ed922ecf25485028c896a55fbdb4722c2066
Ajay Sahu <ajayk@techfoursolutions.com>
2021-05-24 00:07:20 +0530

    Added console log

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const winston = require("winston");
 const express = require("express");
 const config = require("config");
 const app = express();
-
 require("./startup/logging")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
-
+console.log(process.env);
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
